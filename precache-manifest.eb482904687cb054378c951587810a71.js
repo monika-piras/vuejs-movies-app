7ed09ab6904b0d9d86828c7c1ca3789931b65458
Monica Piras <monica.piras.info@gmail.com>
2025-07-26 11:42:18 +0200

    adding dist

---
+++ precache-manifest.eb482904687cb054378c951587810a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a504390a71da183c800c",
+    "url": "/vuejs-movies-app/css/app.a5ea1e7f.css"
+  },
+  {
+    "revision": "0640a9270ec0a195902f",
+    "url": "/vuejs-movies-app/css/chunk-vendors.2c842817.css"
+  },
+  {
+    "revision": "72a5479c37487bd5af4f4e3a1e5ff99e",
+    "url": "/vuejs-movies-app/index.html"
+  },
+  {
+    "revision": "a504390a71da183c800c",
+    "url": "/vuejs-movies-app/js/app.9ee80506.js"
+  },
+  {
+    "revision": "0640a9270ec0a195902f",
+    "url": "/vuejs-movies-app/js/chunk-vendors.3dabdaee.js"
+  },
+  {
+    "revision": "445305f49a5bb02e365a3213baf748e5",
+    "url": "/vuejs-movies-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuejs-movies-app/robots.txt"
+  }
+]);
